9bcc3f049cbafb7c0bf145de872086ad029f7d15
Raybul <93190553+Raybul@users.noreply.github.com>
2022-03-05 20:44:12 +0300

    upd

---
+++ src/assets/icon/smile.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const Smile = () => {
+
+
+    return (
+        <svg width={28} height={28} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 64 64">
+            <circle
+
+                cx="32"
+                cy="32"
+                r="27.758"
+                fill="#f9bf44"
+                data-name="Layer 2"
+            ></circle>
+            <g fill="#382a12" data-name="Layer 1">
+                <path
+                    d="M32 45.165a12.581 12.581 0 01-11.368-7.079 1 1 0 111.795-.882 10.667 10.667 0 0019.146 0 1 1 0 111.795.882A12.581 12.581 0 0132 45.165z"></path>
+                <ellipse cx="21.003" cy="29.745" rx="2.5" ry="2.87"></ellipse>
+                <ellipse cx="42.997" cy="29.745" rx="2.5" ry="2.87"></ellipse>
+            </g>
+        </svg>
+    );
+};
+
+export default Smile;
